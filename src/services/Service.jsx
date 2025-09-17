2bcbe84902d7ef343c9e6fff48e184690f7bca34
marianyarj <marianyarj@gmail.com>
2025-09-17 17:42:44 +0200

    feat: add getAllMedicines function to Service

--- src/services/Service.jsx
+++ src/services/Service.jsx
@@ -1,18 +1,5 @@
 import React from 'react'
 const URL_API_MEDICINE = "http://localhost:3000/medicines/";
-const URL_API_USERS = "http://localhost:3000/users/";
-const URL_API_INTAKES = "http://localhost:3000/intakes/";
-
-
-export async function getAllMedicines() {
-    try {
-        const res = await fetch(URL_API_MEDICINE);
-        console.log(res);
-        return await res.json();
-    } catch (err) {
-        console.error(err);
-    }
-}
 
 
 export async function createMedicine(medicine) {
@@ -22,6 +9,16 @@ export async function createMedicine(medicine) {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(medicine)
         });
+        return await res.json();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export async function getAllMedicines() {
+    try {
+        const res = await fetch(URL_API_MEDICINE);
+        console.log(res);
         return await res.json();
     } catch (err) {
         console.error(err);
