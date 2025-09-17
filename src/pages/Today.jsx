c90ecbab731721aa802c6700b0bbb4356efa1f76
CarmenTajuelo <mcarmentajuelo@gmail.com>
2025-09-17 16:09:44 +0200

    CREATE - Today.jsx - Fill up structure with moke up data + simple return #5

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
 const Today = () => {
+    //Fake Data
+
+    const medications = [
+        { id: 1, name: "Omeprazol", time: "08:00", status: "Tomado" }
+        { id: 2, name: "Paracetamol", time: "14:00", status: "Pendiente" }
+        { id: 3, name: "Ibuprofeno", time: "20:00", status: "Retrasado" }
+    ]
+
+
     return (
-        <div>Today</div>
+        <div>
+            <h2>Hoy</h2>
+            <ul>
+                {medications.map(med => (
+                    <li key={med.id}>
+                        {med.name} - {med.time} - {med.status}
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
