9ec793d105399eacf407b72f7e378f379520bb3c
CarmenTajuelo <mcarmentajuelo@gmail.com>
2025-09-17 16:11:03 +0200

    FIX - Today.jsx - typo commas #5

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Today = () => {
-    //Fake Data
 
+    //Fake Data
     const medications = [
-        { id: 1, name: "Omeprazol", time: "08:00", status: "Tomado" }
-        { id: 2, name: "Paracetamol", time: "14:00", status: "Pendiente" }
+        { id: 1, name: "Omeprazol", time: "08:00", status: "Tomado" },
+        { id: 2, name: "Paracetamol", time: "14:00", status: "Pendiente" },
         { id: 3, name: "Ibuprofeno", time: "20:00", status: "Retrasado" }
     ]
 
