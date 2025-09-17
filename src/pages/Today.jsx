063b3352ebeeceb501362732121fee0f83e49782
CarmenTajuelo <mcarmentajuelo@gmail.com>
2025-09-17 16:42:53 +0200

    UPDATE Today.jsx Update Mock up data with data from ddbb table #5

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -4,9 +4,9 @@ const Today = () => {
 
     //Fake Data
     const medications = [
-        { id: 1, name: "Omeprazol", time: "08:00", status: "Tomado" },
-        { id: 2, name: "Paracetamol", time: "14:00", status: "Pendiente" },
-        { id: 3, name: "Ibuprofeno", time: "20:00", status: "Retrasado" }
+        { id: 1, name: "Omeprazol", description: "Descripcion del medicamento Omeprazol", dosage: "200mg" },
+        { id: 2, name: "Paracetamol", description: "Descripcion del medicamento Paracetamol", dosage: "400mg" },
+        { id: 3, name: "Ibuprofeno", description: "Descripcion del medicamento Ibuprofeno", dosage: "600mg" }
     ]
 
 
