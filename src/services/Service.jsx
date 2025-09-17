a7f30ee2c23ba740710e2ae781e56ed04b3085fc
marianyarj <marianyarj@gmail.com>
2025-09-17 18:39:07 +0200

    feat: add getAllIntakes function to Service

--- src/services/Service.jsx
+++ src/services/Service.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 const URL_API_MEDICINE = "http://localhost:3000/medicines/";
+const URL_API_INTAKES_TODAY = "http://localhost:3000/intakes-today";
 
 
 export async function createMedicine(medicine) {
@@ -28,6 +29,29 @@ export async function getAllMedicines() {
 export async function getMedicineById(id) {
     try {
         const res = await fetch(`${BASE_URL}${id}`);
+        return await res.json();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export async function updateMedicine(id, updatedData) {
+    try {
+        const res = await fetch(`${BASE_URL}${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(updatedData)
+        });
+        return await res.json();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export async function getAllItakes() {
+    try {
+        const res = await fetch(URL_API_INTAKES_TODAY);
+        console.log(res);
         return await res.json();
     } catch (err) {
         console.error(err);
