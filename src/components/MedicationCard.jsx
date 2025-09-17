fae6543cb1bd7c5786c436dce205fe87c79a5931
CarmenTajuelo <mcarmentajuelo@gmail.com>
2025-09-17 17:11:03 +0200

    UPDATE MedicationCard.jsx fixed unused sytles declaration #5

--- src/components/MedicationCard.jsx
+++ src/components/MedicationCard.jsx
@@ -3,12 +3,12 @@ import styles from "./MedicationCard.module.css";
 
 const MedicationCard = ({ name, description, dosage }) => {
     return (
-        <div className="med-card">
+        <div className={styles["med-card"]}>
             <h3>{name}</h3>
             <p>Descripción: {description}</p>
             <p>Dosis: {dosage}</p>
         </div>
-    )
-}
+    );
+};
 
-export default MedicationCard
+export default MedicationCard;
