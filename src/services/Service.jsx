5b64b999d349bb9aee3ac4cf38e3194d54f555a1
Paula Apsé <89433554+Kenhya12@users.noreply.github.com>
2025-09-18 10:31:58 +0200

    feat(service): add CRUD operations for intakes

--- src/services/Service.jsx
+++ src/services/Service.jsx
@@ -48,11 +48,73 @@ export async function updateMedicine(id, updatedData) {
     }
 }
 
+export async function deleteMedicine(id) {
+    try {
+        const res = await fetch(`${URL_API_MEDICINE}${id}`, {
+            method: "DELETE"
+        });
+        return res.ok; // true si se eliminó correctamente
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+    // Crear toma
+export async function createIntake(intake) {
+    try {
+        const res = await fetch(`${URL_API_INTAKES_TODAY}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(intake)
+        });
+        return await res.json();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
 export async function getAllIntakes() {
     try {
         const res = await fetch(URL_API_INTAKES_TODAY);
         console.log(res);
         return await res.json();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Obtener toma por ID
+export async function getIntakeById(id) {
+    try {
+        const res = await fetch(`${URL_API_INTAKES_TODAY}/${id}`);
+        return await res.json();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Actualizar toma
+export async function updateIntake(id, updatedData) {
+    try {
+        const res = await fetch(`${URL_API_INTAKES_TODAY}/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(updatedData)
+        });
+        return await res.json();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Eliminar toma
+export async function deleteIntake(id) {
+    try {
+        const res = await fetch(`${URL_API_INTAKES_TODAY}/${id}`, {
+            method: "DELETE"
+        });
+        return res.ok;
     } catch (err) {
         console.error(err);
     }
