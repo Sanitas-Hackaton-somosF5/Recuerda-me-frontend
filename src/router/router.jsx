49cc0b68d5cea6c5d4160a8107547afb5388baa9
marianyarj <marianyarj@gmail.com>
2025-09-18 11:43:14 +0200

    feat: add MedicineList route

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,7 @@
 import Home from "../pages/Home.jsx";
 import Layout from "../layout/Layout.jsx";
 import Today from "../pages/Today.jsx";
+import MedicineList from "../pages/MedicineList.jsx";
 import { createBrowserRouter } from "react-router-dom";
 
 const routerRecuerda = createBrowserRouter([
@@ -15,6 +16,10 @@ const routerRecuerda = createBrowserRouter([
             {
                 path: "today",
                 element: <Today />,
+            },
+            {
+                path: "/medicineList",
+                element: <MedicineList />,
             }
         ],
     },
